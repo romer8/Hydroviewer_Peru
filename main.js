0e1b11b0a2935b9f67b9141689b39d9f9344dce0
Elkin Romero <gio.busrom@gmail.com>
2020-05-03 01:07:37 -0600

    comments added to define different parts and also the center is now in PERU Lima

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
-var map = L.map('map').setView([4.5709, -74.2973], 6);
+// HYDROVIEWER PERU//
+
+// PART 1: Define Map and also center around the contry of interest in this case PERU//
+
+// a) Add the drainage layer
+var map = L.map('map').setView([-9.046374, -77.042793], 6);
 L.esri.basemapLayer('DarkGray').addTo(map);
 var drainageWMSLayer;
 var url ='https://tethys2.byu.edu/geoserver/peru_hydroviewer/wms'
-// drainageWMSLayer = L.tileLayer.betterWms('https://tethys.byu.edu/geoserver/colombia_hydroviewer/wms', {
 drainageWMSLayer = L.tileLayer.betterWms(url, {
     layers: 'south_america-peru-drainage_line',
     format:'image/png',
     transparent: true
 }).addTo(map);
 
-//part 2
-
+//b)Add the ability to add forecast, historical, seasonal data related to the reach_id
 var marker = null;
-
 map.on("click", function (event) {
     if (marker) {
       map.removeLayer(marker)
