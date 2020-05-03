3f4dca65c0f4d8fa89584e73344021dde8fd4466
Elkin Romero <gio.busrom@gmail.com>
2020-05-03 12:33:46 -0600

    Corrected the handling of clicking poutise the WMS layer

--- L.TileLayer.BetterWMS.js
+++ L.TileLayer.BetterWMS.js
@@ -4,8 +4,7 @@ L.TileLayer.BetterWMS = L.TileLayer.WMS.extend({
     // Triggered when the layer is added to a map.
     //   Register a click listener, then do all the upstream WMS things
     L.TileLayer.WMS.prototype.onAdd.call(this, map);
-    // map.on('click', this.GetFeatureInfo, this);
-    // map.on('click', this.getFeatureInfo, this);
+
   },
 
   onRemove: function (map) {
@@ -37,6 +36,7 @@ L.TileLayer.BetterWMS = L.TileLayer.WMS.extend({
       let url = this._url + L.Util.getParamString(params, this._url, true);
       let reachid = null;
       let drain_area = null;
+      let region = null;
 
       if (url) {
           $.ajax({
@@ -45,74 +45,25 @@ L.TileLayer.BetterWMS = L.TileLayer.WMS.extend({
               url: url,
               info_format: 'application/json',
               success: function (data) {
-                  console.log(data.features[0].properties);
-                  reachid = data.features[0].properties['COMID'];
-                  drain_area = data.features[0].properties['Tot_Drain_'];
-                  console.log(reachid);
-                  // L.popup({ maxWidth: 800}).setContent('hola').openOn(this._map);
-
-                  // FORECAST.graph_f(reachid,"forecast");
+                  if(data['features'].length > 0){
+                    reachid = data.features[0].properties['COMID'];
+                    drain_area = data.features[0].properties['Tot_Drain_'];
+                    region =  data.features[0].properties['region']
+                  }
+                  else{
+                    console.log("Retriving data beyond the WMS layer is not posible");
+                  }
+              },
+              error: function(e){
               }
           });
       }
-      // L.popup({ maxWidth: 800}).setContent('<div id="forecast"><div>').openOn(this._map);
 
-      return [reachid, drain_area]
+      return [reachid, drain_area,region]
 
 
   },
-  getFeatureInfo: function (evt) {
-     // Make an AJAX request to the server and hope for the best
-     var url = this.getFeatureInfoUrl(evt.latlng),
-         showResults = L.Util.bind(this.showGetFeatureInfo, this);
-     $.ajax({
-       url: url,
-       success: function (data, status, xhr) {
-         var err = typeof data === 'string' ? null : data;
-         showResults(err, evt.latlng, data);
-       },
-       error: function (xhr, status, error) {
-         showResults(error);
-       }
-     });
-   },
 
-   getFeatureInfoUrl: function (latlng) {
-     // Construct a GetFeatureInfo request URL given a point
-     var point = this._map.latLngToContainerPoint(latlng, this._map.getZoom()),
-         size = this._map.getSize(),
-
-         params = {
-           request: 'GetFeatureInfo',
-           service: 'WMS',
-           srs: 'EPSG:4326',
-           styles: this.wmsParams.styles,
-           transparent: this.wmsParams.transparent,
-           version: this.wmsParams.version,
-           format: this.wmsParams.format,
-           bbox: this._map.getBounds().toBBoxString(),
-           height: size.y,
-           width: size.x,
-           layers: this.wmsParams.layers,
-           query_layers: this.wmsParams.layers,
-           info_format: 'text/html'
-         };
-
-     params[params.version === '1.3.0' ? 'i' : 'x'] = point.x;
-     params[params.version === '1.3.0' ? 'j' : 'y'] = point.y;
-
-     return this._url + L.Util.getParamString(params, this._url, true);
-   },
-
-   showGetFeatureInfo: function (err, latlng, content) {
-     if (err) { console.log(err); return; } // do nothing if there's an error
-
-     // Otherwise show the content in a popup, or something.
-     L.popup({ maxWidth: 800})
-       .setLatLng(latlng)
-       .setContent(content)
-       .openOn(this._map);
-   }
 });
 
 L.tileLayer.betterWms = function (url, options) {
